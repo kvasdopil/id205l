161b17dd40c9e6ad99793f87967183ce93d03e7c
Alexey Guskov <lexag@mail.ru>
2020-03-29 16:44:53 +0200

    fix

---
+++ README.md
@@ -0,0 +1,121 @@
+# ID205L smart watch custom firmware WIP 
+
+## Photos: 
+Fccid: https://fccid.io/2AHFT228/Internal-Photos/Internal-Photo-4438078
+Opened device: https://photos.google.com/share/AF1QipMEgaVUau3sA0FcNTVeyZKY0TScTzwYiXO7fe6Rh2gIK8x5G0z2D5svC86-koTstQ?key=MGM4Y1JZT2xTaGxvSmY0MWdkMnpBYVZNd3lES3JR
+PCB photos: https://photos.google.com/share/AF1QipO3eowMmGWax-Yk8IF-ml1vgUQFByVLIt-XGVHYddveYpcckNRNJQFuctqWAHlYUA?key=MEE2aWFhakNvVFg1VUpoTENOeEF6RU82OEZBY0ln
+Chip markings: https://photos.google.com/share/AF1QipNFV63yPYG3SIgiTFkugtkMYC76d3eq6xieDRYmCEP69MpZUu-oj6QuwDR9ZJyVDg?key=bS1uQldlSGN4Wmh2alh4bENydW9YTGV1aTg3djFB
+
+## How to open:
+i'm pretty sure the easiest way to open it would be using a hot gun and suction cup to remove the glass. What i did is:
+1) removed the button (just pull it out firmly)
+2) used a hairdryer to soften the glue inside and removed the grey cover around the screen
+3) used a hairdryer to soften the glue behind the screen and caaaaaaaarefully pulled it up
+
+## How to build firmware
+Prebuilt .hex files: (see the repo). Beware, DFU is not working!
+
+- Checkout espruino sources
+- ./scripts/provision.py NRF52340DK
+- copy build/IS205L.py to espruino/boards
+
+To build bootloader:
+```
+make clean && \
+    DFU_UPDATE_BUILD=1 \
+    BOARD=ID205L \
+    RELEASE=1 \
+    DEFINES="-DUARTE1_EASYDMA_MAXCNT_SIZE=16 -I./targetlibs/nrf5x_15/components/libraries/bootloader/ -I/targetlibs/nrf5x_15/components/libraries/bootloader/dfu"  \
+    BOOTLOADER=1 \
+    make
+```
+To build device code
+```
+make clean && \
+    DFU_UPDATE_BUILD=1 \
+    BOARD=ID205L \
+    RELEASE=1 \
+    DEFINES="-DUARTE1_EASYDMA_MAXCNT_SIZE=16 -DNRF52"  \
+    make
+```
+
+Change this:
+```
+DEFINES += DNRF52 -DNRF52840_XXAA
+```
+to this:
+```
+DEFINES += DNRF52 -DNRF52840_XXAA
+```
+in `NRF52.make`
+
+## How to flash
+
+Connect to J-Link to device to following pads:
+VCC - VCC
+G - GND
+DIO - DIO  
+C - CLK
+
+Use softdevice 140 v6.0.0
+
+## What we do know
+### Chip: NRF52840
+
+Pinout
+0 - 
+1 
+2 - related to LCD?
+3
+4
+5 - SCL device 0x1f?, related to LCD?
+6 - 
+7 - HEART_SENSOR? SDA device 0x44
+8 - HEART_SENSOR? SCL device 0x44
+9  - 
+10 - 
+11 - 
+12 - MEMORY_WP
+13 - 
+14 = heart senssor backlight aka LED1
+15 - related to LCD? related to HEART?
+16 = side button aka BTN1
+17 - HEART_SENSOR_ENABLE
+18 - (DEVICE RESET), related LCD?, related to HEART?
+19 - MEMORY_SO, related to HEART?
+20 = MOTOR
+21 = MEMORY_CS
+22 = BACKLIGHT
+23 - 
+24 - 
+25 - CHARGING, related to HEART?
+26 - 
+27 - SDA device 0x1f?
+28 - 
+29 - 
+30 - BACKLIGHT2
+31 - related to LCD?
+32
+33
+34
+35
+36
+37
+38
+39
+40
+
+These pins are connected to wires going to heart sensor and button
+- 15, 16(btn), 17, 18, 19, 25
+
+This pins are connected to display and touch sensor
+- 2, 15, 18, 30, 31
+
+### Touch sensor
+IT7259, photos: https://photos.app.goo.gl/u1DJjaMRU4kKJ2W87
+
+### Accelerometer
+Unknown markings are B271 VS35
+
+## Ask questions
+https://gitter.im/nRF51822-Arduino-Mbed-smart-watch/Lobby
---
+++ build/ID205L.py
@@ -0,0 +1,122 @@
+#!/bin/false
+# This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+#
+# Copyright (C) 2013 Gordon Williams <gw@pur3.co.uk>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# ----------------------------------------------------------------------------------------
+# This file contains information for a specific board - the available pins, and where LEDs,
+# Buttons, and other in-built peripherals are. It is used to build documentation as well
+# as various source and header files for Espruino.
+# ----------------------------------------------------------------------------------------
+
+import pinutils;
+
+info = {
+ 'name' : "ID205L",
+ 'link' :  [ "https://www.nordicsemi.com/eng/Products/Bluetooth-low-energy/nRF52-DK" ],
+ 'espruino_page_link' : 'ID205L',
+  # This is the PCA10036
+ 'default_console' : "EV_SERIAL1",
+ 'default_console_tx' : "D6",
+ 'default_console_rx' : "D8",
+ 'default_console_baudrate' : "9600",
+ 'variables' : 12500, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'bootloader' : 1,
+ 'binary_name' : 'espruino_%v_id205l.hex',
+ 'build' : {
+   'optimizeflags' : '-Os',
+   'libraries' : [
+     'BLUETOOTH',
+     'NET',
+     'GRAPHICS',
+#     'NFC',
+     'NEOPIXEL'
+   ],
+   'makefile' : [
+     'DEFINES += -DCONFIG_GPIO_AS_PINRESET', # Allow the reset pin to work
+     #'DEFINES += -DBOARD_PCA10056',
+     #'DEFINES += -DNRF_USB=1 -DUSB',
+     'NRF_SDK15=1',
+     'DFU_PRIVATE_KEY=targets/nrf5x_dfu/dfu_private_key.pem',
+   ]
+ }
+};
+
+
+chip = {
+  'part' : "NRF52840",
+  'family' : "NRF52",
+  'package' : "QFN48",
+  'ram' : 256,
+  'flash' : 1024,
+  'speed' : 64,
+  'usart' : 2,
+  'spi' : 3,
+  'i2c' : 2,
+  'adc' : 1,
+  'dac' : 0,
+  'saved_code' : {
+    'address' : ((246 - 10) * 4096), # Bootloader takes pages 248-255, FS takes 246-247
+    'page_size' : 4096,
+    'pages' : 10,
+    'flash_available' : 1024 - ((31 + 8 + 2 + 10)*4) # Softdevice uses 31 pages of flash, bootloader 8, FS 2, code 10. Each page is 4 kb.
+  },
+};
+
+devices = {
+  'BTN1' : { 'pin' : 'D16', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
+  'LED1' : { 'pin' : 'D14' }, # Pin negated in software
+};
+
+# left-right, or top-bottom order
+board = {
+  'left' : [ 'VDD', 'VDD', 'RESET', 'VDD','5V','GND','GND','','','D3','D4','D28','D29','D30','D31'],
+  'right' : [
+     'D27', 'D26', 'D2', 'GND', 'D25','D24','D23', 'D22','D20','D19','',
+     'D18','D17','D16','D15','D14','D13','D12','D11','',
+     'D10','D9','D8','D7','D6','D5','D21','D1','D0'],
+  '_notes' : {
+    'D6' : "Serial console RX",
+    'D8' : "Serial console TX"
+  }
+};
+board["_css"] = """
+#board {
+  width: 528px;
+  height: 800px;
+  top: 0px;
+  left : 200px;
+  background-image: url(img/NRF528DK.jpg);
+}
+#boardcontainer {
+  height: 900px;
+}
+
+#left {
+    top: 219px;
+    right: 466px;
+}
+#right {
+    top: 150px;
+    left: 466px;
+}
+
+.leftpin { height: 17px; }
+.rightpin { height: 17px; }
+""";
+
+def get_pins():
+  pins = pinutils.generate_pins(0,47) # 48 General Purpose I/O Pins.
+  pinutils.findpin(pins, "PD0", True)["functions"]["XL1"]=0;
+  pinutils.findpin(pins, "PD1", True)["functions"]["XL2"]=0;
+  pinutils.findpin(pins, "PD16", True)["functions"]["NEGATED"]=0;
+
+  # everything is non-5v tolerant
+  for pin in pins:
+    pin["functions"]["3.3"]=0;
+  #The boot/reset button will function as a reset button in normal operation. Pin reset on PD21 needs to be enabled on the nRF52832 device for this to work.
+  return pins
