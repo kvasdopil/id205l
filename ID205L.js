06d2ec91e31ee743a9e097070c61fbaf55a4002e
Alexey Guskov <lexag@mail.ru>
2020-03-29 18:11:47 +0200

    pin monitor code cleanup

--- ID205L.js
+++ ID205L.js
@@ -156,7 +156,7 @@ setWatch(() => {
 
 const pinMonitor = (mode) => {
   const regs = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
@@ -164,10 +164,12 @@ const pinMonitor = (mode) => {
   ];
 
   let pr = {};
+  regs.forEach(r => {
+    Pin(r).mode(mode);
+  });
 
-  let int = setInterval(() => {
+  setInterval(() => {
     regs.forEach(r => {
-      Pin(r).mode(mode);
       const val = digitalRead(r);
       if (val != pr[r]) {
         console.log(r, val);
