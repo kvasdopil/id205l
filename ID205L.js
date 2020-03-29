12459d3b4d878c227f9c91d3091ede7945c4d0d1
Alexey Guskov <lexag@mail.ru>
2020-03-29 20:13:40 +0200

    added data from HX3600 datasheet to src

--- ID205L.js
+++ ID205L.js
@@ -89,12 +89,58 @@ digitalPulse(HEART_BACKLIGHT, 1, 100);
 
 backlight(2);
 
-digitalWrite(HEART_SENSOR_ENABLE, 1);
-const heartSensor = new I2C();
-heartSensor.setup({ sda: HEART_SDA, scl: HEART_SCL });
+// === heart rate sensor functions ===
 
-// =======
-// Demo of the button 
+const HEART_SENSOR_DEVICEID = 0x44;
+const heartI2C = new I2C();
+heartI2C.setup({ sda: HEART_SDA, scl: HEART_SCL });
+const heartSensor = {
+  enable: () => digitalWrite(HEART_SENSOR_ENABLE, 1),
+  disable: () => digitalWrite(HEART_SENSOR_ENABLE, 0),
+  read: (reg, length) => {
+    heartI2C.writeTo(HEART_SENSOR_DEVICEID, reg);
+    return heartI2C.readFrom(HEART_SENSOR_DEVICEID, length);
+  }
+};
+
+/* HX3600 registers 
+addr   value  desc
+0x00 - 0x22 - device ID
+0x01 - 0x01 - reserved
+0x02 - 0x11 - functions enabled (should be 0x33)
+0x03 - 0x00 - reserved (should be 0x8f)
+0x04 - 0x10 - LED1 phase config
+0x05 - 0x20 - LED2 phase config
+0x06 - 0x50 - INT1 config
+0x07 - 0x07 - INT1 config
+0x08 - 0x00 - INT1 config
+0x09 - 0x02 - sleep enabled
+
+0x14 - 0x00 - ps offset
+0x15 - 0x00 - hrs offset
+0x16 - 0x40 - ps interval
+
+0xA0 - 0x00 - hrs data out
+0xA1 - 0x00 - hrs data out
+0xA2 - 0x00 - hrs data out
+
+0xA3 - 0x00 - als data out
+0xA4 - 0x00 - als data out
+0xA5 - 0x00 - als data out
+
+0xA6 - 0x00 - ps1 data out
+0xA7 - 0x00 - ps1 data out
+0xA8 - 0x00 - ps1 data out
+
+0xA9 - 0x00 - als2 data out
+0xAA - 0x00 - als2 data out
+0xAB - 0x00 - als2 data out
+
+0xC0 - 0x86 - led driver config
+*/
+
+// ======= demo of the button ===
+
 setWatch(() => {
   digitalPulse(HEART_BACKLIGHT, 1, 100);
   vibrate(100);
@@ -211,3 +257,8 @@ const tryDisplay = () => {
     });
   }, BTN1, { edge: 'rising', debounce: 50, repeat: true });
 };
+
+/// ====
+
+heartSensor.enable();
+console.log(heartSensor.read(0, 16).map(i => Number(i).toString(16)));
