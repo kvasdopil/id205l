d8400e7ad7b481cadba94b56aa2d9fd89fe1ffcc
Alexey Guskov <lexag@mail.ru>
2020-05-03 00:32:06 +0200

    cleanup

--- utils/upload_images.sh
+++ utils/upload_images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "var s = require('Storage'); s.eraseAll();" > .images.js
-cat ./img/*.i >> .images.js
-yarn cli .images.js
-rm -f .images.js
+cat ./img/*.i >> ./temp/images.js
+yarn cli /temp/images.js
+rm -f /temp/images.js
